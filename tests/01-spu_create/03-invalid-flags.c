d85c165e308126f6b3ac1bd07376c1b05e3ebd8c
Jeremy Kerr <jk@ozlabs.org>
2007-05-03 17:33:36 +1000

    Add invalid flags test for spu_create
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

---
+++ tests/01-spu_create/03-invalid-flags.c
@@ -0,0 +1,42 @@
+#include <test/utils.h>
+#include <test/spu_syscalls.h>
+#include <stdlib.h>
+#include <errno.h>
+
+const unsigned long spu_create_all_flags =
+	SPU_CREATE_EVENTS_ENABLED |
+	SPU_CREATE_GANG |
+	SPU_CREATE_NOSCHED |
+	SPU_CREATE_ISOLATE |
+#if 0
+	SPU_CREATE_AFFINITY_SPU 
+	SPU_CREATE_AFFINITY_MEM
+#endif
+	0;
+
+int main(void)
+{
+	char *name = name_spu_context(NULL);
+	int bit, ctx;
+
+	for (bit = 0; bit < 32; bit++) {
+		unsigned long flags = 1ul << bit;
+		if (flags & spu_create_all_flags)
+			continue;
+		printf("trying flags: 0x%08lx\n", flags);
+		ctx = spu_create(name, flags, 0);
+		if (ctx != -1) {
+			fprintf(stderr, "spu_create() accepted invalid flags: "
+					"0x%08lx\n", flags);
+			return EXIT_FAILURE;
+		}
+		if (errno != EINVAL) {
+			fprintf(stderr, "spu_create(): wrong errno (%d) with "
+					"invalid flags: 0x%08lx\n",
+					errno, flags);
+			return EXIT_FAILURE;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
