272e2007d53bb8848796950249eb5a119138fd87
Michael Ellerman <michael@ellerman.id.au>
2007-08-24 11:18:55 +1000

    Add SPU coredump tests
    These tests test the kernel-side SPU coredump support.
    
    01-coredump.sh requires the fix to binfmt_elf.c to correctly
    handle foffset:
    
    de5db25249ca70f295850740f4f536b59ae1b672
    
    02-coredump.sh requires fixes to the SPU coredump code to
    write certain values as ASCII, as required by GDB.
    (not yet committed in mainline)
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>

---
+++ tests/10-coredump/01-coredump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ulimit -c unlimited
+
+pid=$(coredump)
+
+corefile="core.$pid"
+
+if [[ ! -f $corefile ]]; then
+	echo "Can't find corefile? ulimit? core_pattern?" >&2
+	exit 2
+fi
+
+load_addr=$(readelf -l $corefile | awk '/LOAD/ {print $2; exit}')
+load_addr=$(($load_addr))
+
+dd if=$corefile of=out.bin bs=1 skip=$load_addr count=4
+echo -en "\x7fELF" | cmp out.bin
+
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
+
+rm -f $corefile out.bin
+
+exit 0
