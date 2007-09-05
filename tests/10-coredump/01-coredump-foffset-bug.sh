9a37904f9451fa68d54ebc40d2df35c9cc2e3f2a
Jeremy Kerr <jk@ozlabs.org>
2007-09-05 17:08:23 +1000

    GPLification of spufs-testsuite
    Add GPL text in COPYING, and add headers to sources.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- tests/10-coredump/01-coredump-foffset-bug.sh
+++ tests/10-coredump/01-coredump-foffset-bug.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+#
+# Testsuite for the Linux SPU filesystem
+#
+# Copyright (C) IBM Corporation, 2007
+#
+# Author: Michael Ellerman <michael@ellerman.id.au>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 ulimit -c unlimited
 
