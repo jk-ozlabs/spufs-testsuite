03f3442e1b07a49cb15b7f426a2523d210d1bf87
Jeremy Kerr <jk@ozlabs.org>
2008-07-01 11:34:46 +1000

    02-spu_create: test file sizes
    Add tests for file sizes
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- tests/02-spu_create/05-files.c
+++ tests/02-spu_create/05-files.c
@@ -28,44 +28,50 @@
 #include <errno.h>
 #include <string.h>
 
+#include <test/hw.h>
+
+#define SPU_DMA_INFO_SIZE (5 * sizeof(uint64_t) + 16 * 4 * sizeof(uint64_t))
+#define SPU_PROXYDMA_INFO_SIZE (3 * sizeof(uint64_t) + 8 * 4 * sizeof(uint64_t))
+
 struct spufs_file {
 	const char	*name;
 	mode_t		mode;
+	size_t		size;
 } spufs_files[] = {
-	{ "capabilities",	0100444 },
-	{ "mem",		0100666 },
-	{ "regs",		0100666 },
-	{ "mbox",		0100444 },
-	{ "ibox",		0100444 },
-	{ "wbox",		0100222 },
-	{ "mbox_stat",		0100444 },
-	{ "ibox_stat",		0100444 },
-	{ "wbox_stat",		0100444 },
-	{ "signal1",		0100666 },
-	{ "signal2",		0100666 },
-	{ "signal1_type",	0100666 },
-	{ "signal2_type",	0100666 },
-	{ "cntl",		0100666 },
-	{ "fpcr",		0100666 },
-	{ "lslr",		0100444 },
-	{ "mfc",		0100666 },
-	{ "mss",		0100666 },
-	{ "npc",		0100666 },
-	{ "srr0",		0100666 },
-	{ "decr",		0100666 },
-	{ "decr_status",	0100666 },
-	{ "event_mask",		0100666 },
-	{ "event_status",	0100444 },
-	{ "psmap",		0100666 },
-	{ "phys-id",		0100666 },
-	{ "object-id",		0100666 },
-	{ "mbox_info",		0100444 },
-	{ "ibox_info",		0100444 },
-	{ "wbox_info",		0100444 },
-	{ "dma_info",		0100444 },
-	{ "proxydma_info",	0100444 },
-	{ "tid",		0100444 },
-	{ "stat",		0100444 },
+	{ "capabilities",	0100444, },
+	{ "mem",		0100666, LS_SIZE },
+	{ "regs",		0100666, 16 * 128 },
+	{ "mbox",		0100444, },
+	{ "ibox",		0100444, },
+	{ "wbox",		0100222, },
+	{ "mbox_stat",		0100444, sizeof(uint32_t) },
+	{ "ibox_stat",		0100444, sizeof(uint32_t) },
+	{ "wbox_stat",		0100444, sizeof(uint32_t) },
+	{ "signal1",		0100666, },
+	{ "signal2",		0100666, },
+	{ "signal1_type",	0100666, },
+	{ "signal2_type",	0100666, },
+	{ "cntl",		0100666, },
+	{ "fpcr",		0100666, 16 },
+	{ "lslr",		0100444, },
+	{ "mfc",		0100666, },
+	{ "mss",		0100666, },
+	{ "npc",		0100666, },
+	{ "srr0",		0100666, },
+	{ "decr",		0100666, },
+	{ "decr_status",	0100666, },
+	{ "event_mask",		0100666, },
+	{ "event_status",	0100444, },
+	{ "psmap",		0100666, 0x20000 },
+	{ "phys-id",		0100666, },
+	{ "object-id",		0100666, },
+	{ "mbox_info",		0100444, sizeof(uint32_t) },
+	{ "ibox_info",		0100444, sizeof(uint32_t) },
+	{ "wbox_info",		0100444, sizeof(uint32_t) },
+	{ "dma_info",		0100444, SPU_DMA_INFO_SIZE },
+	{ "proxydma_info",	0100444, SPU_PROXYDMA_INFO_SIZE },
+	{ "tid",		0100444, },
+	{ "stat",		0100444, },
 	{ NULL,			0    }
 };
 
@@ -90,6 +96,12 @@ int main(void)
 		if (stat.st_mode != file->mode) {
 			fprintf(stderr, "file %s: mode %o doesn't match %o\n",
 					file->name, stat.st_mode, file->mode);
+			return EXIT_FAILURE;
+		}
+
+		if (file->size != stat.st_size) {
+			fprintf(stderr, "file %s: size %ld doesn't match %zu\n",
+					file->name, stat.st_size, file->size);
 			return EXIT_FAILURE;
 		}
 	}
